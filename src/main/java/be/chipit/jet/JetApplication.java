47483309e9da2161fea0006cfcaf798fd027cf23
Philippe Schottey <phschotte@gmail.com>
2023-12-30 09:02:22 +0100

    feat: catch global exceptions

--- src/main/java/be/chipit/jet/JetApplication.java
+++ src/main/java/be/chipit/jet/JetApplication.java
@@ -1,14 +1,21 @@
 package be.chipit.jet;
 
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.shell.command.annotation.CommandScan;
 
 @SpringBootApplication
 @CommandScan(value = "be.chipit.jet.adapters.command")
+@Slf4j
 public class JetApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(JetApplication.class, args);
+        try {
+            SpringApplication.run(JetApplication.class, args);
+        } catch (Exception e) {
+            log.error("Application error", e);
+        }
     }
 }
