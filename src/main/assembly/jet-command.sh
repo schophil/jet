66b0645c4596029074e719d5bdffdfab2c092399
Philippe Schottey <schophil@icloud.com>
2025-04-22 15:34:27 +0200

    fix: copy on x11 does not work

--- src/main/assembly/jet-command.sh
+++ src/main/assembly/jet-command.sh
@@ -10,7 +10,7 @@ fi
 if [ -f /tmp/jet.copy ]; then
     command=$(cat /tmp/jet.copy)
     rm /tmp/jet.copy
-    if command -v xsl >/dev/null 2>&1; then
+    if command -v xsel >/dev/null 2>&1; then
         echo "$command" | xsel -i -b
     elif command -v wl-copy >/dev/null 2>&1; then
         echo "$command" | wl-copy
