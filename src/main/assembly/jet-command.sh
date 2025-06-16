10df2bd71f52fb3d4a9cdcbf5ea71f792c021553
Philippe Schottey <schophil@icloud.com>
2025-06-16 14:52:34 +0200

    feat: support wsl and windows clipboard

--- src/main/assembly/jet-command.sh
+++ src/main/assembly/jet-command.sh
@@ -10,7 +10,9 @@ fi
 if [ -f /tmp/jet.copy ]; then
     command=$(cat /tmp/jet.copy)
     rm /tmp/jet.copy
-    if command -v xsel >/dev/null 2>&1; then
+    if uname -r | grep -q 'microsoft'; then
+        echo "$command" | clip.exe
+    elif command -v xsel >/dev/null 2>&1; then
         echo "$command" | xsel -i -b
     elif command -v wl-copy >/dev/null 2>&1; then
         echo "$command" | wl-copy
